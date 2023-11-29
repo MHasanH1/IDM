9a0d8f6a43e00eeef0fb40acda9bc9bcb1aa5d37
Mohammad Hasan Heydari <121676852+MHasanH1@users.noreply.github.com>
2023-11-29 22:58:52 +0330

    Add files via upload

---
+++ Download.py
@@ -0,0 +1,13 @@
+import os
+import urllib.request
+
+url = input("Enter Your URL: ")
+path = input("Enter Your Path: ")
+fileName = input("Enter Your File Name: ")
+fileType = input("Enter Your File Type: ")
+fullName = fileName + '.' + fileType
+
+print("Downloading...")
+os.chdir(path)
+urllib.request.urlretrieve(url, fullName)
+print("Downloaded succesfully in" + path + "as" + fullName)
